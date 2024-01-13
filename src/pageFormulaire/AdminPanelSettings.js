f0fc7343c007cd3a2158f5281d73bf2e68620ec4
Ammoniak007 <67445876+Ammoniak007@users.noreply.github.com>
2024-01-13 15:16:25 +0100

    maj admin

--- src/pageFormulaire/AdminPanelSettings.js
+++ src/pageFormulaire/AdminPanelSettings.js
@@ -75,13 +75,13 @@ export default function AdminPanelSettings({ accidentData }) {
         <div className="frameStyle-style">
             <h3>Administration des droits</h3>
 
-            <h3>Crée un nouvelle utilisateur</h3>
+            <h3>Créer un nouvelle utilisateur</h3>
 
             <TextFieldP id='nameLogin' label="Adresse email" onChange={setnameLogin} defaultValue={nameLogin}></TextFieldP>
 
             <TextFieldP id='adminPanelSettingsmdp' label="Mot de passe" onChange={setadminPanelSettingsmdp} defaultValue={adminPanelSettingsmdp}></TextFieldP>
 
-            <h3>Donnez les accès administration du site:</h3>
+            <h3>Donner les accès administration du site:</h3>
 
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
 
@@ -92,7 +92,7 @@ export default function AdminPanelSettings({ accidentData }) {
 
             </Box>
 
-            <h3>Donnez les accès conseiller en prévention:</h3>
+            <h3>Donner les accès conseiller en prévention:</h3>
 
             <Box sx={{ display: 'flex', justifyContent: 'center', marginLeft: '120px', marginRight: '120px' }}>
                 <ControlLabelAdminP id="boolCortil" label="Cortil" onChange={(boolCortilCoche) => {
@@ -151,7 +151,7 @@ export default function AdminPanelSettings({ accidentData }) {
 
             </Box>
 
-            <h3>Donnez les accès uniquement consulté les ATs:</h3>
+            <h3>Donner les accès consulter les ATs:</h3>
 
             <Box sx={{ display: 'flex', justifyContent: 'center', marginLeft: '120px', marginRight: '120px' }}>
                 <ControlLabelAdminP id="boolCortilr" label="Cortil" onChange={(boolCortilrCoche) => {
@@ -240,36 +240,51 @@ export default function AdminPanelSettings({ accidentData }) {
 
             <h3>Actions</h3>
             <Box sx={{ display: 'flex', justifyContent: 'center', marginLeft: '120px', marginRight: '120px' }}>
-                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } ,
-                fontSize: '1rem', // Taille de police de base
-                              // Utilisation de Media Queries pour ajuster la taille de police
-                              '@media (min-width: 750px)': {
-                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
-                            },
-                            '@media (max-width: 550px)': {
-                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
-                            },
-            }} variant="contained"> Supprimer toutes les données</Button>
-                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } ,
-                fontSize: '1rem', // Taille de police de base
-                              // Utilisation de Media Queries pour ajuster la taille de police
-                              '@media (min-width: 750px)': {
-                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
-                            },
-                            '@media (max-width: 550px)': {
-                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
-                            },
-             }} variant="contained"> Archivez toutes les données</Button>
-                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
-                fontSize: '1rem', // Taille de police de base
-                              // Utilisation de Media Queries pour ajuster la taille de police
-                              '@media (min-width: 750px)': {
-                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
-                            },
-                            '@media (max-width: 550px)': {
-                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
-                            },
-             }} variant="contained"> Consulté les archives</Button>
+                <Button type="submit" sx={{
+                    margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
+                    fontSize: '1rem', // Taille de police de base
+                    // Utilisation de Media Queries pour ajuster la taille de police
+                    '@media (min-width: 750px)': {
+                        fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                    },
+                    '@media (max-width: 550px)': {
+                        fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                    },
+                }} variant="contained"> Supprimer toutes les données</Button>
+                <Button type="submit" sx={{
+                    margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
+                    fontSize: '1rem', // Taille de police de base
+                    // Utilisation de Media Queries pour ajuster la taille de police
+                    '@media (min-width: 750px)': {
+                        fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                    },
+                    '@media (max-width: 550px)': {
+                        fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                    },
+                }} variant="contained"> Archiver toutes les données</Button>
+                <Button type="submit" sx={{
+                    margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
+                    fontSize: '1rem', // Taille de police de base
+                    // Utilisation de Media Queries pour ajuster la taille de police
+                    '@media (min-width: 750px)': {
+                        fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                    },
+                    '@media (max-width: 550px)': {
+                        fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                    },
+                }} variant="contained"> Consulter les archives</Button>
+
+                <Button type="submit" sx={{
+                    margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
+                    fontSize: '1rem', // Taille de police de base
+                    // Utilisation de Media Queries pour ajuster la taille de police
+                    '@media (min-width: 750px)': {
+                        fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                    },
+                    '@media (max-width: 550px)': {
+                        fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                    },
+                }} variant="contained"> Consulter les utilisateurs</Button>
             </Box>
 
 
