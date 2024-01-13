1795428008bee4e64cfefc14a42f35ac2323df45
Ammoniak007 <67445876+Ammoniak007@users.noreply.github.com>
2024-01-13 15:03:43 +0100

    adminpanel

--- src/pageFormulaire/AdminPanelSettings.js
+++ src/pageFormulaire/AdminPanelSettings.js
@@ -240,11 +240,36 @@ export default function AdminPanelSettings({ accidentData }) {
 
             <h3>Actions</h3>
             <Box sx={{ display: 'flex', justifyContent: 'center', marginLeft: '120px', marginRight: '120px' }}>
-
-                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } }} variant="contained"> Supprimer toutes les données</Button>
-                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } }} variant="contained"> Archivez toutes les données</Button>
-
-
+                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } ,
+                fontSize: '1rem', // Taille de police de base
+                              // Utilisation de Media Queries pour ajuster la taille de police
+                              '@media (min-width: 750px)': {
+                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                            },
+                            '@media (max-width: 550px)': {
+                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                            },
+            }} variant="contained"> Supprimer toutes les données</Button>
+                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' } ,
+                fontSize: '1rem', // Taille de police de base
+                              // Utilisation de Media Queries pour ajuster la taille de police
+                              '@media (min-width: 750px)': {
+                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                            },
+                            '@media (max-width: 550px)': {
+                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                            },
+             }} variant="contained"> Archivez toutes les données</Button>
+                <Button type="submit" sx={{margin: '10px', backgroundColor: '#84a784', '&:hover': { backgroundColor: 'green' },
+                fontSize: '1rem', // Taille de police de base
+                              // Utilisation de Media Queries pour ajuster la taille de police
+                              '@media (min-width: 750px)': {
+                                fontSize: '1rem', // Taille de police plus grande pour les écrans plus larges
+                            },
+                            '@media (max-width: 550px)': {
+                                fontSize: '0.5rem', // Taille de police plus petite pour les écrans plus étroits
+                            },
+             }} variant="contained"> Consulté les archives</Button>
             </Box>
 
 
