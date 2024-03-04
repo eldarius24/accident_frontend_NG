76db34f12d99fe211a1aed049ba390d69a02a9ab
Eldarius <94144689+eldarius24@users.noreply.github.com>
2024-03-04 18:38:09 +0100

    Mise à jour de CountNumberAccident.js pour trier les accidents par date et attribuer des numéros de groupe et d'entreprise.

--- src/Model/CountNumberAccident.js
+++ src/Model/CountNumberAccident.js
@@ -1,25 +1,44 @@
 /**
- * Counts the number of accidents and assigns group and enterprise numbers to each accident.
- * @param {Array} accidents - The array of accidents.
- * @returns {Array} - The array of accidents with assigned group and enterprise numbers.
+ * Compte le nombre d'accidents et attribue un numéro de groupe et d'entreprise à chaque accident.
+ * @param {Array} accidents - Le tableau des accidents.
+ * @returns {Array} - Le tableau des accidents avec les numéros de groupe et d'entreprise attribués.
  */
 export default function CountNumberAccidentGroupe(accidents) {
+    // Trie les accidents par date dans l'ordre croissant
     accidents.sort((a, b) => new Date(a.DateHeureAccident) - new Date(b.DateHeureAccident));
 
     let groupe = 0;
     let entrepriseNumbers = {};
+    let currentYear = null;
+
+    // Supprime les accidents nuls ou vides du tableau
+    accidents = accidents.filter(accident => accident !== null && Object.keys(accident).length !== 0);
 
     accidents.forEach((accident) => {
-        groupe++;
-        accident.numeroGroupe = groupe;
+        const accidentYear = new Date(accident.DateHeureAccident).getFullYear();
+        if (accidentYear !== currentYear) {
+            // Si l'année de l'accident est différente de l'année en cours, réinitialise les numéros de groupe et d'entreprise
+            currentYear = accidentYear;
+            groupe = 1;
+            entrepriseNumbers = {};
+        } else {
+            // Incrémente le numéro de groupe
+            groupe++;
+        }
+
+        // Attribue le numéro de groupe à l'accident
+        accident.numeroGroupe = `${accidentYear}-${groupe}`;
 
         const entrepriseName = accident.entrepriseName;
+        // Incrémente le numéro d'entreprise pour le nom d'entreprise spécifique
         entrepriseNumbers[entrepriseName] = (entrepriseNumbers[entrepriseName] || 0) + 1;
-        accident.numeroEntreprise = entrepriseNumbers[entrepriseName];
+        // Attribue le numéro d'entreprise à l'accident
+        accident.numeroEntreprise = `${accidentYear}-${entrepriseNumbers[entrepriseName]}`;
     });
 
+    // Affiche les accidents après le tri et l'attribution des numéros
     accidents.forEach((accident) => {
-        console.log("accidents after sort  => ", accident.numeroGroupe, "->", accident.numeroEntreprise, "->", accident.DateHeureAccident);
+        console.log("accidents après le tri  => ", accident.numeroGroupe, "->", accident.numeroEntreprise, "->", accident.DateHeureAccident);
     });
 
     return accidents;
