fcf9b63e487d33e192486fef9d14498629905414
Ammoniak007 <67445876+Ammoniak007@users.noreply.github.com>
2024-10-10 17:39:01 +0200

    maj snackbare

--- src/_composants/CustomSnackbar.js
+++ src/_composants/CustomSnackbar.js
@@ -19,7 +19,7 @@ const CustomSnackbar = ({ open, handleClose, message, severity }) => {
           width: '100%',
           fontSize: '1rem',  // Augmenter la taille du texte
           padding: '10px',      // Ajouter du padding pour rendre l'alert plus grande
-          backgroundColor: severity === 'success' ? 'transparent' : 'transparent',  // Couleur de fond différente selon la gravité
+          backgroundColor: severity === 'success' ? '#eeffee' : '#ffe8e8',  // Couleur de fond différente selon la gravité
           color: severity === 'success' ? '#257525' : '#ff0000',  // Couleur du texte différente selon la gravité
           outline: severity === 'success' ? '5px solid green' : '5px solid red',  // Bordure extérieure différente selon la gravité
         }}
