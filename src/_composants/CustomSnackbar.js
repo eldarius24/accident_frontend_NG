28e7720ecf8505e1ffda89f0717182dd04b84ad2
Ammoniak007 <67445876+Ammoniak007@users.noreply.github.com>
2024-10-13 15:24:09 +0200

    maj snackbar

--- src/_composants/CustomSnackbar.js
+++ src/_composants/CustomSnackbar.js
@@ -5,7 +5,7 @@ const CustomSnackbar = ({ open, handleClose, message, severity }) => {
   return (
     <Snackbar
       anchorOrigin={{
-        vertical: 'top',
+        vertical: 'top', // Reste en bas pour le positionnement
         horizontal: 'left',
       }}
       open={open}
@@ -17,13 +17,13 @@ const CustomSnackbar = ({ open, handleClose, message, severity }) => {
         severity={severity}
         sx={{
           width: '100%',
-          fontSize: '1rem',  // Augmenter la taille du texte
-          padding: '10px',      // Ajouter du padding pour rendre l'alert plus grande
-          backgroundColor: severity === 'success' ? '#eeffee' : '#ffe8e8',  // Couleur de fond différente selon la gravité
-          color: severity === 'success' ? '#257525' : '#ff0000',  // Couleur du texte différente selon la gravité
-          outline: severity === 'success' ? '3px solid green' : '5px solid red',  // Bordure extérieure différente selon la gravité
+          fontSize: '1rem', // Augmenter la taille du texte
+          padding: '10px', // Ajouter du padding pour rendre l'alert plus grande
+          backgroundColor: severity === 'success' ? '#eeffee' : '#ffe8e8', // Couleur de fond différente selon la gravité
+          color: severity === 'success' ? '#257525' : '#ff0000', // Couleur du texte différente selon la gravité
+          outline: severity === 'success' ? '3px solid green' : '5px solid red', // Bordure extérieure différente selon la gravité
           position: 'relative', 
-          top: '50px',
+          top: '60px', // Décaler la Snackbar de 40px vers le haut
         }}
       >
         {message}
